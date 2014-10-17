22766642c10f99642f7b693abc8e66d7deb8fd3b
Hazen <Hazen2@users.noreply.github.com>
2014-10-16 19:09:14 -1000

    Update MyFirstProgram.java
    Added escape sequence \n

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,8 +1,13 @@
-// This is my first java program as well as GitHub repository. Hope I'm doing it right!
-public class MyFirstProgram
+// Showing the difference between println and print
+public class Printlines
 {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-	}
+  public static void main(String[] args)
+  {
+    System.out.println("println will advance the cursor to the next line after output");
+    System.out.print("However, print will ");
+    System.out.print("not advance to the next line after output.");
+    
+    // Discuss escape sequence /n
+    System.out.print("You may also use an escape sequence like so: \nApples \nGrapes \nTurtles");
+  }
 }
